2faea65242dc7ff942650ce1b1c5c11cee98170e
jahaziel2903 <55322568+jahaziel2903@users.noreply.github.com>
2019-11-24 16:40:49 -0600

    Add files via upload

---
+++ Shapes2D.java
@@ -0,0 +1,19 @@
+
+public abstract class Shapes2D{
+
+	 public Vector Posicion = new Vector();//x, y  
+	 public Vector Velocidad = new Vector();//dx, dy   
+	 public Vector gravedad = new Vector(0,-9.81);//gravedad es un vector de velocidad 
+	 public int masa;//Masa del cuerpo sólido 
+	 
+     public Shapes2D(int x_pos, int y_pos, int x_vel, int y_vel ,int masa){  
+    	 this.Posicion.x = x_pos;   
+    	 this.Posicion.y = y_pos;  
+    	 this.Velocidad.x = x_vel;  
+    	 this.Velocidad.y = y_vel;  
+    	 this.masa = masa;  
+    	}
+     
+     
+     
+}
