84c62772afe1f5277dfdb48addec6e499ce0a429
jahaziel2903 <55322568+jahaziel2903@users.noreply.github.com>
2019-11-24 16:45:47 -0600

    Update Shapes2D.java

--- Shapes2D.java
+++ Shapes2D.java
@@ -4,7 +4,7 @@ public abstract class Shapes2D{
 	 public Vector Posicion = new Vector();//x, y  
 	 public Vector Velocidad = new Vector();//dx, dy   
 	 public Vector gravedad = new Vector(0,-9.81);//gravedad es un vector de velocidad 
-	 public int masa;//Masa del cuerpo sólido 
+	 public int masa;//Masa del cuerpo sÃ³lido 
 	 
      public Shapes2D(int x_pos, int y_pos, int x_vel, int y_vel ,int masa){  
     	 this.Posicion.x = x_pos;   
@@ -13,7 +13,20 @@ public abstract class Shapes2D{
     	 this.Velocidad.y = y_vel;  
     	 this.masa = masa;  
     	}
+	
+          public double getX_posicion() {
+    	 	return Posicion.x;
+     }
      
+    	 public double getY_posicion() {
+    		 return Posicion.y;
+     }
+       public void setX_posicion(double x) {
+    	 	 Posicion.x = x;;
+     }
      
+     public void setY_posicion(double y) {
+    	  	Posicion.y = y;
+     }
      
 }
