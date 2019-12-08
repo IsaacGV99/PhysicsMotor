864db43774e067b900b1126b9ce8d9dddfd4ada6
jahaziel2903 <55322568+jahaziel2903@users.noreply.github.com>
2019-12-08 01:57:48 -0600

    Update Collisions.java

--- Collisions.java
+++ Collisions.java
@@ -15,4 +15,23 @@ public class Collisions {
 		   r1.getHeight() + r1.position.getY() > r2.position.getY();
 	}
 
+	     public static void ManejoColision_CIRCLE_CIRCLE(Shapes2D shape) { // maneja la colision   
+		 double e = 1; // coeficiente restitucion
+		 Vector new_direction;   
+		 new_direction = this.Posicion.subVectors(shape.Posicion);
+		 new_direction = new_direction.normalize();     
+
+		 double velocity1 = this.Velocidad.dot(new_direction);
+		 double velocity2 = shape.Velocidad.dot(new_direction);  
+
+		 double new_velocity1 = ( ((this.masa - e*shape.masa)*velocity1) + ((1+e)*shape.masa*velocity2) ) / (this.masa + shape.masa); 
+		 double new_velocity2 = ( ((shape.masa - e*this.masa)*velocity2) + ((1+e)*this.masa*velocity1) ) / (this.masa + shape.masa); 
+
+		 Vector v1Final = this.Velocidad.suma(new_direction.escalar(new_velocity1 - velocity1));  
+		 Vector v2Final = shape.Velocidad.suma(new_direction.escalar(new_velocity2-velocity2));   
+		 this.Velocidad.x = v1Final.x;  
+		 this.Velocidad.y = v1Final.y;  
+		 shape.Velocidad.x = v2Final.x; 
+		 shape.Velocidad.y = v2Final.y;     
+		 } 
 }
