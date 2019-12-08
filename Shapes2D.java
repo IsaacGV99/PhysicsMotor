dbf56578e385398674f42e43cc83e3f8bfcd9a36
Jose Isaac Gallegos Velica <39529793+IsaacGV99@users.noreply.github.com>
2019-12-08 11:22:00 -0600

    Update Shapes2D.java

--- Shapes2D.java
+++ Shapes2D.java
@@ -1,32 +1,59 @@
 
 public abstract class Shapes2D{
 
-	 public Vector Posicion = new Vector();//x, y  
-	 public Vector Velocidad = new Vector();//dx, dy   
-	 public Vector gravedad = new Vector(0,-9.81);//gravedad es un vector de velocidad 
-	 public int masa;//Masa del cuerpo sólido 
+	public Vector position = new Vector();//x, y  
+	public Vector speed = new Vector();//dx, dy   
+	public Vector gravity = new Vector(0,-9.81);//gravedad es un vector de velocidad 
+	public int mass;//Masa del cuerpo sólido 
 	 
-     public Shapes2D(int x_pos, int y_pos, int x_vel, int y_vel ,int masa){  
-    	 this.Posicion.x = x_pos;   
-    	 this.Posicion.y = y_pos;  
-    	 this.Velocidad.x = x_vel;  
-    	 this.Velocidad.y = y_vel;  
-    	 this.masa = masa;  
-    	}
+	public Shapes2D(int x_pos, int y_pos, int x_speed, int y_speed ,int masa){  
+	 	this.position.setX(x_pos);     
+	 	this.position.setY(y_pos);  
+	 	this.speed.setX(x_speed);  
+	 	this.speed.setY(y_speed);  
+	 	this.mass = mass; 
+	}
 	
-          public double getX_posicion() {
-    	 	return Posicion.x;
-     }
+	public Shapes2D(int x_pos, int y_pos) {
+		this(x_pos, y_pos, 0, 0, 1);
+	}
+	
+	public Vector getPosition() {
+    	 	return this.position;
+     	}
      
-    	 public double getY_posicion() {
-    		 return Posicion.y;
-     }
-       public void setX_posicion(double x) {
-    	 	 Posicion.x = x;;
-     }
+    	 public Vector getSpeed() {
+    		return this.speed;
+     	}
+	
+     	public void setPosition(int x, int y) {
+    	 	this.position.setX(x);
+		this.position.setY(y);
+     	}
+	
+	public void setPosition(Vector newPosition) {
+    	 	this.position.setX(newPosition.getX());
+		this.position.setY(newPosition.getY());
+     	}
      
-     public void setY_posicion(double y) {
-    	  	Posicion.y = y;
-     }
-     
+	public void setSpeed(int x, int y) {
+    	 	this.speed.setX(x);
+		this.speed.setY(y);
+     	}
+	
+	public void setSpeed(Vector newSpeed) {
+    	 	this.speed.setX(newSpeed.getX());
+		this.speed.setY(newSpeed.getY());
+     	}
 }
+    
+    public int getMass() {
+    	return this.mass;
+    }
+    
+    public void setMass(int mass) {
+    	this.mass = mass;
+    }
+    
+}
+
